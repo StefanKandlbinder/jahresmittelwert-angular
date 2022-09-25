bed780666718d1ab9e8c686ac2ab17dd86f2d582
Stefan Kandlbinder <stefan.kandlbinder@gmail.com>
2022-09-25 16:55:11 +0200

    angular update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,11 @@
-import { HtmlAstPath } from '@angular/compiler';
-import { AfterContentChecked, AfterViewInit, Component, ElementRef, OnChanges, OnInit, SimpleChanges, ViewChild } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  OnChanges,
+  OnInit,
+  SimpleChanges,
+  ViewChild,
+} from '@angular/core';
 import { take } from 'rxjs';
 import { ICard } from './card/card.interface';
 import { MeasurementService } from './measurement/measurement.service';
@@ -10,30 +16,33 @@ import { createUrls } from './utilities/utilities';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit, OnChanges {
   @ViewChild('periodButtonGroup')
   periodButtonGroup!: ElementRef<HTMLElement>;
 
   station: ICard = {
-    "stationId": "S415",
-    "stationShort": "-",
-    "measurand": "NO2",
-    "mean": "TMW",
-    "value": "0.00",
-    "unit": "µg/m³",
-    "dateFrom": 0,
-    "dateTo": 0,
-  }
+    stationId: 'S415',
+    stationShort: '-',
+    measurand: 'NO2',
+    mean: 'TMW',
+    value: '0.00',
+    unit: 'µg/m³',
+    dateFrom: 0,
+    dateTo: 0,
+  };
   stations!: Station[];
   selectedStation: string = this.station.stationId;
-  measurands = ["NO2", "PM10kont", "PM25kont"];
+  measurands = ['NO2', 'PM10kont', 'PM25kont'];
   days = 0;
   loading = false;
-  title = "Tagesdurchschnitt";
+  title = 'Tagesdurchschnitt';
 
-  constructor(private measurementService: MeasurementService, private stationsService: StationsService) {
+  constructor(
+    private measurementService: MeasurementService,
+    private stationsService: StationsService
+  ) {
     this.stations = this.stationsService.getStations();
   }
 
@@ -48,27 +57,28 @@ export class AppComponent implements OnInit, OnChanges {
 
   onChangePolluter(event: any) {
     this.station.measurand = event.target.value;
-    this.update()
+    this.update();
   }
 
   onChangeStation(event: any) {
     this.station.stationId = event.target.value;
     this.selectedStation = this.station.stationId;
-    this.update()
+    this.update();
   }
 
   onHandlePeriod(event: any, days: number, mean: string) {
     this.days = days;
     this.station.mean = mean;
 
-    this.periodButtonGroup.nativeElement.querySelectorAll("jmw-button").forEach(button => {
-      if (button.id === event.target.parentElement.id) {
-        event.target.parentElement.classList.add("active")
-      }
-      else {
-        button.classList.remove("active");
-      }
-    })
+    this.periodButtonGroup.nativeElement
+      .querySelectorAll('jmw-button')
+      .forEach((button) => {
+        if (button.id === event.target.parentElement.id) {
+          event.target.parentElement.classList.add('active');
+        } else {
+          button.classList.remove('active');
+        }
+      });
 
     this.update();
   }
@@ -78,40 +88,52 @@ export class AppComponent implements OnInit, OnChanges {
 
     switch (this.days) {
       case 0:
-        this.title = "Aktueller Wert"
-        this.station.mean = "HMW"
+        this.title = 'Aktueller Wert';
+        this.station.mean = 'HMW';
         break;
       case 1:
-        this.title = "Tagesdurchschnitt"
-        this.station.mean = "HMW"
+        this.title = 'Tagesdurchschnitt';
+        this.station.mean = 'HMW';
         break;
       case 7:
-        this.title = "Wochendurchschnitt"
-        this.station.mean = "TMW"
+        this.title = 'Wochendurchschnitt';
+        this.station.mean = 'TMW';
         break;
       case 31:
-        this.title = "Monatsdurchschnitt"
-        this.station.mean = "TMW"
+        this.title = 'Monatsdurchschnitt';
+        this.station.mean = 'TMW';
         break;
     }
 
-    this.measurementService.getData(createUrls(this.days, this.station.stationId, this.station.measurand, false), this.station.mean).pipe(
-      take(1)
-    ).subscribe({
-      next: (measurements) => {
-        this.loading = false;
-        this.station = this.measurementService.getDataStation(measurements, this.station.mean, this.days);
-      },
-      error: (error: any) => {
-        throw error;
-      },
-      complete: () => {
-        // writeStationsData(station, component, tmpMeasurements[0]);
-        // writeMeanData(station, component, mittelwert);
+    this.measurementService
+      .getData(
+        createUrls(
+          this.days,
+          this.station.stationId,
+          this.station.measurand,
+          false
+        ),
+        this.station.mean
+      )
+      .pipe(take(1))
+      .subscribe({
+        next: (measurements) => {
+          this.loading = false;
+          this.station = this.measurementService.getDataStation(
+            measurements,
+            this.station.mean,
+            this.days
+          );
+        },
+        error: (error: any) => {
+          throw error;
+        },
+        complete: () => {
+          // writeStationsData(station, component, tmpMeasurements[0]);
+          // writeMeanData(station, component, mittelwert);
 
-        console.log("COMPLETED");
-      }
-    });
-
+          console.log('COMPLETED');
+        },
+      });
   }
 }
