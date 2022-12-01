aef8ab80bb92244d56349f4fd9a5e345aff041a3
Stefan Kandlbinder <stefan.kandlbinder@gmail.com>
2022-12-01 14:30:42 +0100

    tailwind v3

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,17 @@
 module.exports = {
-  mode: "jit",
-  purge: ["./src/**/*.html", "./src/**/*.ts"],
-  darkMode: false, // or 'media' or 'class'
+  content: ["./src/**/*.html", "./src/**/*.ts"],
   theme: {
     fontFamily: {
-      'sans': ['Bitter', 'ui-sans-serif', 'system-ui'],
-      'serif': ['ui-serif', 'Georgia'],
-      'mono': ['ui-monospace', 'SFMono-Regular'],
-      'display': ['Oswald'],
-      'body': ['"Bitter"']
-     },
-     minWidth: ['spacing']
+      sans: ["Bitter", "ui-sans-serif", "system-ui"],
+      serif: ["ui-serif", "Georgia"],
+      mono: ["ui-monospace", "SFMono-Regular"],
+      display: ["Oswald"],
+      body: ['"Bitter"'],
+    },
+    minWidth: ["spacing"],
   },
   variants: {
-    extend: {}
+    extend: {},
   },
-  plugins: []
+  plugins: [],
 };
